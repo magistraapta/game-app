8b775950a3a979f5aadcd78d9f5848d89553b7d5
magistraapta <magsitraagis@gmail.com>
2024-01-04 12:26:27 +0800

    Initial Commit

---
+++ game-app/game_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  game_appApp.swift
+//  game-app
+//
+//  Created by Magistra Apta on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct game_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
